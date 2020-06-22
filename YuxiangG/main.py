7b783c234740a95c03f38b80127bb8e209ebaa82
GYX97 <413998386@qq.com>
2020-06-21 20:14:50 -0500

    update

---
+++ YuxiangG/main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import cv2
+
+#from camera import Camera
+#import structure
+import processor
+import features
+
+def dino():
+    # Dino
+    img1 = cv2.imread('images/viff.002.ppm')
+    img2 = cv2.imread('images/viff.001.ppm')
+    pts1, pts2 = features.find_correspondence_points(img1, img2)
+    points1 = processor.cart2hom(pts1)
+    points2 = processor.cart2hom(pts2)
+
+    fig, ax = plt.subplots(1, 2)
+    ax[0].autoscale_view('tight')
+    ax[0].imshow(cv2.cvtColor(img1, cv2.COLOR_BGR2RGB))
+    ax[0].plot(points1[0], points1[1], 'r.')
+    ax[1].autoscale_view('tight')
+    ax[1].imshow(cv2.cvtColor(img2, cv2.COLOR_BGR2RGB))
+    ax[1].plot(points2[0], points2[1], 'r.')
+    plt.show()
+
+    height, width, ch = img1.shape
+    intrinsic = np.array([  # for dino
+        [2360, 0, width / 2],
+        [0, 2360, height / 2],
+        [0, 0, 1]])
+
+    return points1, points2, intrinsic
+
+
+points1, points2, intrinsic = dino()
